12a8c430f6beb9ff7406e6c30084371bb21f7480
Mark Biesheuvel <mail@markbiesheuvel.nl>
2015-01-27 21:46:03 +0100

    Improvemonts based on Google Pagespeed Insights

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,61 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         imagemin: {
             source: {
-                files: [{
-                    expand: true,
-                    cwd: 'images/source/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'images/compressed/'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/source/',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: 'images/compressed/'
+                    }
+                ]
+            }
+        },
+        compass: {
+            build: {
+                options: {
+                    sassDir: 'css',
+                    cssDir: 'css/compiled',
+                    environment: 'production'
+                }
+            },
+        },
+        watch: {
+            config: {
+                files: ['Gruntfile.js', 'config.rb'],
+                tasks: ['build:css'],
+                options: {
+                    reload: true
+                }
+            },
+            sass: {
+                files: 'css/**/*.scss',
+                tasks: ['build:css'],
+                options: {
+                    interrupt: true,
+                    sourcemap: true
+                }
+            },
+            livereload: {
+                files: ['index.php', 'templates/*.html.twig', 'css/*.css'],
+                options: {
+                    livereload: true
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    // Load the plugins
+    require('load-grunt-tasks')(grunt);
 
-    //grunt.registerTask('default', ['']);
+    grunt.registerTask(
+        'build:css',
+        'Compile CSS',
+        ['compass:build']
+    );
+
+    grunt.registerTask('default', ['watch']);
 
 };
